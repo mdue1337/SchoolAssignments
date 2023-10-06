e7e45735f97038b027b6a97f4e042cdec66a56cf
mdue1337 <martin@familiendue.dk>
2023-10-06 09:32:09 +0200

    .

--- SortingAlgoritms/SortingAlgoritms/SortingAlgoritms/Program.cs
+++ SortingAlgoritms/SortingAlgoritms/SortingAlgoritms/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SortingAlgoritms
 {
@@ -13,20 +11,44 @@ namespace SortingAlgoritms
             int[] ints = { 12, 16, 2, 28, 19};
             BubbleSort(ints);
 
+            Console.WriteLine("Bubblesort");
+
             for (int i = 0; i < ints.Length; i++)
             {
                 Console.WriteLine($"{i+1}: {ints[i]}");
             }
 
-            Console.WriteLine();
+            Console.WriteLine("\nInsertion sort");
 
-            List<int> ints2 = ints.ToList();
-            int value = 13;
-            InsertionSort(ints2, value);
+            int[] ints2 = ints.ToArray();
+
+            InsertionSort(ints2);
 
             for (int i = 0; i < ints.Length; i++)
             {
                 Console.WriteLine($"{i + 1}: {ints2[i]}");
+            }
+
+            int[] values = { 2, 1, 4, 4, 3, 5, 6, 9, 6, 8 };
+
+            Console.WriteLine("\nQuicksort");
+
+            int[] ints3 = QuickSort(values, 0, values.Length - 1);
+
+            Console.WriteLine();
+
+            for (int i = 0; i < ints3.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}: {ints3[i]}");
+            }
+
+            Console.WriteLine("\nMergeSort");
+
+            int[] MergeSort = SortArray(values, 0, values.Length - 1);
+
+            for (int i = 0; i < MergeSort.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}: {MergeSort[i]}");
             }
 
             Console.ReadKey();
@@ -57,19 +79,113 @@ namespace SortingAlgoritms
             }
         }
 
-        public static void InsertionSort(List<int> list, int value)
+        public static void InsertionSort(int[] arr)
         {
-            int n = list.Count;
-            int i = n - 1;
-
-            while (i >= 0 && value < list[i])
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
             {
-                i--;
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j =- 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+
+        public static void MergeArray(int[] array, int left, int middle, int right)
+        {
+            var leftArrayLength = middle - left + 1;
+            var rightArrayLength = right - middle;
+            var leftTempArray = new int[leftArrayLength];
+            var rightTempArray = new int[rightArrayLength];
+            int i, j;
+
+            for (i = 0; i < leftArrayLength; ++i)
+                leftTempArray[i] = array[left + i];
+            for (j = 0; j < rightArrayLength; ++j)
+                rightTempArray[j] = array[middle + 1 + j];
+
+            i = 0;
+            j = 0;
+            int k = left;
+
+            while (i < leftArrayLength && j < rightArrayLength)
+            {
+                if (leftTempArray[i] <= rightTempArray[j])
+                {
+                    array[k++] = leftTempArray[i++];
+                }
+                else
+                {
+                    array[k++] = rightTempArray[j++];
+                }
             }
 
-            Console.WriteLine($"Inserted value at index: {i + 2}");
+            while (i < leftArrayLength)
+            {
+                array[k++] = leftTempArray[i++];
+            }
 
-            list.Insert(i + 1, value);
+            while (j < rightArrayLength)
+            {
+                array[k++] = rightTempArray[j++];
+            }
+        }
+
+        public static int[] SortArray(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+
+                SortArray(array, left, middle);
+                SortArray(array, middle + 1, right);
+
+                MergeArray(array, left, middle, right);
+            }
+
+            return array;
+        }
+
+        public static int[] QuickSort(int[] array, int leftIndex, int rightIndex)
+        {
+            var i = leftIndex;
+            var j = rightIndex;
+            var pivot = array[leftIndex];
+            Console.WriteLine($"Current pivot point: {pivot}");
+
+            while (i <= j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    Console.WriteLine($"Switched: {array[i]} to {array[j]}");
+                    i++;
+                    j--;
+                }
+            }
+
+            if (leftIndex < j)
+                QuickSort(array, leftIndex, j);
+            if (i < rightIndex)
+                QuickSort(array, i, rightIndex);
+
+            return array;
         }
     }
 }
